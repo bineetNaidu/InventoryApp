ca9d1cf42d2f2bcf16dba1ecf70e894c7773cca9
bineetNaidu <bineet.s.naidu@gmail.com>
2021-05-19 09:23:55 +0530

    🎉 update(connecting db with db url)

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -12,29 +12,21 @@ if (process.env.NODE_ENV !== 'production') {
 const PORT = process.env.PORT || 4242;
 
 (async () => {
-  if (!process.env.DB_HOST) throw new Error('ENV(DB_HOST) is undefined');
-
-  if (!process.env.DB_USERNAME)
-    throw new Error('ENV(DB_USERNAME) is undefined');
-
-  if (!process.env.DB_PASSWORD)
-    throw new Error('ENV(DB_PASSWORD) is undefined');
-
   if (!process.env.JWT_TOKEN) throw new Error('ENV(JWT_TOKEN) is undefined');
+  if (!process.env.DATABASE_URL)
+    throw new Error('ENV(DATABASE_URL) is undefined');
 
   try {
     const connection: Connection = await createConnection({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: 5432,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
+      url: process.env.DATABASE_URL,
       database: 'inventory_app',
       entities: [User, Comment, Item, Manufacturer],
     });
     connection.isConnected && console.log('>>> DB is Connected <<<');
   } catch (error) {
     console.log('!!!! >> ', error.message, '<< !!!!');
+    process.exit(1);
   }
 
   // **** Listeners ****
