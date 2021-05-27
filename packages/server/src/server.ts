6527a0da1aaaba5c73682ef133544058051f5d11
bineetNaidu <bineet.s.naidu@gmail.com>
2021-05-27 23:45:42 +0530

    ❇️ add constants

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -5,8 +5,9 @@ import { User } from './models/User.model';
 import { Comment } from './models/Comment.model';
 import { Item } from './models/Items.model';
 import { Manufacturer } from './models/Manufacturer.model';
+import { ___prod___ } from './utils/constants';
 
-if (process.env.NODE_ENV !== 'production') {
+if (!___prod___) {
   dotenv.config();
 }
 const PORT = process.env.PORT || 4242;
@@ -22,8 +23,8 @@ const PORT = process.env.PORT || 4242;
       url: process.env.DATABASE_URL,
       database: 'inventory_app',
       entities: [User, Comment, Item, Manufacturer],
-      logging: process.env.NODE_ENV !== 'production',
-      synchronize: process.env.NODE_ENV !== 'production',
+      logging: !___prod___,
+      synchronize: !___prod___,
     });
     connection.isConnected && console.log('>>> DB is Connected <<<');
   } catch (error) {
@@ -36,7 +37,7 @@ const PORT = process.env.PORT || 4242;
     console.log('-----------------------------------------');
     console.log('>>> INVENTORY API SERVER HAS STARTED <<<');
     console.log('-----------------------------------------');
-    if (process.env.NODE_ENV !== 'production') {
+    if (!___prod___) {
       console.log('>> visit: http://localhost:' + PORT);
     }
   });
