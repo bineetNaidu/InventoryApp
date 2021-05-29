8436cce7c5ed530a62d7b58a5ae79eb66f28660c
bineetNaidu <bineet.s.naidu@gmail.com>
2021-05-29 21:37:36 +0530

    🐛 Fixed ( item update returing result logic )

--- packages/server/src/routers/v1/items/update.ts
+++ packages/server/src/routers/v1/items/update.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { getConnection } from 'typeorm';
 import { Item } from '../../../models/Items.model';
 
 export const updateItem = async (req: Request, res: Response) => {
@@ -12,17 +13,23 @@ export const updateItem = async (req: Request, res: Response) => {
     throw new Error('The Item with the given ID was Not Found');
   }
 
-  const updatedItem = await Item.update(item.id, {
-    name,
-    price,
-    has_warranty,
-    purchase_location,
-    info,
-    item_type,
-  });
+  const updatedItem = await getConnection()
+    .createQueryBuilder()
+    .update(Item)
+    .set({
+      name,
+      price,
+      has_warranty,
+      purchase_location,
+      info,
+      item_type,
+    })
+    .where('id = :id', { id: item.id })
+    .returning('*')
+    .execute();
 
   res.json({
-    item: updatedItem.raw,
+    item: updatedItem.raw[0],
     updated_item_id: item.id,
     updated: true,
     success: true,
