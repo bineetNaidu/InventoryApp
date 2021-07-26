af8d58a849d5304b494735a95262733a7023ac85
bineetNaidu <bineet.s.naidu@gmail.com>
2021-07-26 21:03:40 +0530

    🎉 add UI to main tab screen

--- packages/app/screens/tabs/Main.tsx
+++ packages/app/screens/tabs/Main.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, ScrollView } from 'react-native';
+import { Text } from 'react-native-elements';
+import { AntDesign } from '@expo/vector-icons';
 
 const Main = () => {
   return (
     <View style={styles.container}>
-      <Text>Main Tab</Text>
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.filterBtn}>
+          <AntDesign name="API" size={35} color="#fff" />
+        </TouchableOpacity>
+        <Text style={styles.headerTxt} h3>
+          Your Items
+        </Text>
+      </View>
+      <ScrollView>{/* the Items */}</ScrollView>
     </View>
   );
 };
@@ -13,6 +23,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#5A4FD3',
+    paddingLeft: 15,
+    paddingRight: 15,
+    paddingTop: 15,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  filterBtn: {
+    padding: 10,
+    borderRadius: 12,
+    backgroundColor: '#442BAA',
+  },
+  headerTxt: {
+    color: '#D6D1E8',
   },
 });
 export default Main;
