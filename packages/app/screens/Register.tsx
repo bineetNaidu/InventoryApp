3fe0b5ca90fa31e1a1193c4a89f0f28659dc9739
bineetNaidu <bineet.s.naidu@gmail.com>
2021-07-26 15:00:53 +0530

    ❇️ implement Register UI

--- packages/app/screens/Register.tsx
+++ packages/app/screens/Register.tsx
@@ -1,6 +1,8 @@
 import { StackNavigationProp } from '@react-navigation/stack';
-import React, { FC, useLayoutEffect } from 'react';
-import { StyleSheet, Text, KeyboardAvoidingView } from 'react-native';
+import React, { FC, useLayoutEffect, useState, useCallback } from 'react';
+import { Button, Input, Text } from 'react-native-elements';
+import { AntDesign, FontAwesome } from '@expo/vector-icons';
+import { StyleSheet, KeyboardAvoidingView, ScrollView } from 'react-native';
 
 type RegisterScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -12,6 +14,16 @@ interface Props {
 }
 
 const Register: FC<Props> = ({ navigation }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [inventory_location, setInventoryLocation] = useState('');
+
+  const handleRegister = useCallback(() => {
+    navigation.navigate('Home');
+  }, [navigation]);
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerStyle: {
@@ -24,7 +36,70 @@ const Register: FC<Props> = ({ navigation }) => {
 
   return (
     <KeyboardAvoidingView style={styles.container}>
-      <Text>Register</Text>
+      <ScrollView style={styles.fields}>
+        <Text
+          h3
+          style={{ color: '#fff', fontStyle: 'italic', marginBottom: 30 }}
+        >
+          Create Your Account
+        </Text>
+
+        <Input
+          style={styles.input}
+          label="Email"
+          autoFocus
+          keyboardType="email-address"
+          labelStyle={styles.labels}
+          leftIcon={<AntDesign name="mail" size={24} color="#fff" />}
+          value={email}
+          onChangeText={(email) => setEmail(email)}
+        />
+        <Input
+          style={styles.input}
+          label="Username"
+          labelStyle={styles.labels}
+          leftIcon={<AntDesign name="user" size={24} color="#fff" />}
+          value={username}
+          onChangeText={(username) => setUsername(username)}
+        />
+        <Input
+          style={styles.input}
+          label="Inventory Location"
+          labelStyle={styles.labels}
+          leftIcon={<FontAwesome name="globe" size={24} color="#fff" />}
+          value={inventory_location}
+          onChangeText={(inventory_location) =>
+            setInventoryLocation(inventory_location)
+          }
+        />
+        <Input
+          secureTextEntry
+          label="Password"
+          labelStyle={styles.labels}
+          style={styles.input}
+          leftIcon={<AntDesign name="lock1" size={24} color="#fff" />}
+          value={password}
+          onChangeText={(password) => setPassword(password)}
+        />
+        <Input
+          style={styles.input}
+          label="Confirm Password"
+          labelStyle={styles.labels}
+          leftIcon={<AntDesign name="lock1" size={24} color="#fff" />}
+          value={passwordConfirm}
+          onChangeText={(passwordConfirm) =>
+            setPasswordConfirm(passwordConfirm)
+          }
+          onSubmitEditing={handleRegister}
+        />
+        <Button title="Register!" onPress={handleRegister} />
+        <Button
+          title="Have an Account!"
+          type="clear"
+          onPress={() => navigation.navigate('Login')}
+          titleStyle={{ ...styles.labels, textDecorationLine: 'underline' }}
+        />
+      </ScrollView>
     </KeyboardAvoidingView>
   );
 };
@@ -32,7 +107,22 @@ const Register: FC<Props> = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    height: '100%',
     backgroundColor: '#5A4FD3',
+  },
+  input: {
+    paddingLeft: 10,
+    color: '#fff',
+  },
+  labels: {
+    color: '#fff',
+    letterSpacing: 0.5,
+  },
+  fields: {
+    flex: 1,
+    width: '80%',
+    alignSelf: 'center',
+    marginTop: 40,
   },
 });
 
