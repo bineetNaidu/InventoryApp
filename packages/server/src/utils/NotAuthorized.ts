3a8d8951e5a6313edb08a51d3a992d134e7b5119
bineetNaidu <bineet.s.naidu@gmail.com>
2021-08-03 21:19:34 +0530

    ✨ add Express Custom Error Handler

---
+++ packages/server/src/utils/NotAuthorized.ts
@@ -0,0 +1,14 @@
+import CustomError from './CustomError';
+
+export class NotAuthorizedError extends CustomError {
+  statusCode = 401;
+  constructor() {
+    super('Forbidden');
+
+    Object.setPrototypeOf(this, NotAuthorizedError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: 'Not Authorized' }];
+  }
+}
