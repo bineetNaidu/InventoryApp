c321cbc83aca32a669cf5d606093f3615977c663
bineetNaidu <bineet.s.naidu@gmail.com>
2021-08-04 11:56:46 +0530

    🎉 config item_types routers

--- packages/server/src/routers/index.ts
+++ packages/server/src/routers/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { commentsRoutes } from './comments';
 import { inventoryLocationsRoutes } from './inventoryLocations';
 import { itemRoutes } from './items';
+import { itemTypesRoutes } from './itemTypes';
 import { manufacturerRoutes } from './manufacturers';
 import { userRoutes } from './users';
 
@@ -13,5 +14,6 @@ r.use('/items', itemRoutes);
 r.use('/comments', commentsRoutes);
 r.use('/inventory_location', inventoryLocationsRoutes);
 r.use('/manufacturers', manufacturerRoutes);
+r.use('/item_types', itemTypesRoutes);
 
 export { r as v1Routes };
