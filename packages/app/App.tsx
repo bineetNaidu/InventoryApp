b1d4e70fcc548f35108640007fa009261a4582f4
bineetNaidu <bineet.s.naidu@gmail.com>
2021-08-13 22:30:08 +0530

    ❇️ initial inventory locations fetchs

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,19 +1,40 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Product from './screens/Product';
 import Login from './screens/Login';
 import Register from './screens/Register';
 import Home from './screens/Home';
+import { useInventoryLocationStore } from './lib/inventoryLocation.store';
+import { ax } from './lib/axios';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
+  const setInventories = useInventoryLocationStore((s) => s.setInventories);
+
+  const fetchAllInventories = async () => {
+    const { data } = await ax.get<{
+      inventoryLocations: InventoryLocationsType[];
+      length: number;
+      success: boolean;
+    }>('/inventory_location');
+
+    if (data.success) {
+      setInventories(data.inventoryLocations);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllInventories();
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
+        initialRouteName="Product"
         headerMode="float"
         screenOptions={{
           headerTitleAlign: 'center',
