eed4de459755d6886ef2f31e97251994765d44eb
bineetNaidu <bineet.s.naidu@gmail.com>
2021-10-04 10:59:06 +0530

    🎉 add manufacturers seeds migrations

--- packages/server/src/migrations/1628001689303-seeds.ts
+++ packages/server/src/migrations/1628001689303-seeds.ts
@@ -17,6 +17,12 @@ export class seeds1628001689303 implements MigrationInterface {
 				('Food', now(), now()),
 				('Drink', now(), now()),
 				('Other', now(), now());
+
+			insert into manufacturers (name, description, contacts, website, logo_url, created_at, updated_at)
+			values 
+				('Amazon', 'Amazon.com, Inc. is an American multinational technology company which focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence.', 'https://www.amazon.com/gp/help/customer/display.html?nodeId=GSD587LKW72HKU2V', 'https://www.amazon.com/', 'http://pngimg.com/uploads/amazon/amazon_PNG11.png', now(), now()),
+				('Flipkart', 'Flipkart is an online shopping platform for consumer electronics, home, and personal care.', 'https://www.flipkart.com/helpcentre', 'https://www.flipkart.com/', 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png', now(), now()),
+				('Apple', 'Apple Inc. is an American multinational technology company that makes personal computers and mobile devices.', 'https://support.apple.com/', 'https://www.apple.com/', 'https://www.apple.com/ac/globalnav/6/en_US/images/be15095f-5a20-57d0-ad14-cf4c638e223a/globalnav_apple_image__cxwwnrj0urau_large.svg', now(), now());
 		`);
   }
 
